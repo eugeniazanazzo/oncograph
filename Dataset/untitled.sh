ce6b70842cf9daa72201849c2baccc297a95c9af
Eugenia Zanazzo <eugenia.zanazzo@gmail.com>
2018-01-31 16:54:34 +0100

    31/01 aggiornamento

--- Dataset/untitled.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..3}
-do
-./generate <BRCA_SM7 >test$i.pl 
-swipl -f "test$i.pl" -g "tell('output$i'),induce([train],P),test(P,[test],LL,AUROC,ROC,AUPRC,PR),write(P),nl,write(LL),nl,write(AUROC),nl,write(AUPRC),told." -t halt.
-done
